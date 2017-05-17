e780fee2bde7fa036c00f7736dcbf893ee3c1deb
ali yeysides <ali.yeysides@gmail.com>
2017-05-17 15:16:33 -0400

    updates primary header with time functionality

--- src/app/shared/primary-header/primary-header.component.ts
+++ src/app/shared/primary-header/primary-header.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'psp-primary-header',
   templateUrl: './primary-header.component.html',
@@ -7,10 +9,35 @@ import {Component, OnInit} from '@angular/core';
 })
 export class PrimaryHeaderComponent implements OnInit {
 
+  public year = moment().year();
+  public date = moment().format('MMM D');
+  public day = moment().format('ddd');
+
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  public getGreetingTime() {
+    let greeting = null,
+        m = moment();
+
+    if(!m || !m.isValid()) { return; }
+
+    let split_afternoon = 12;
+    let split_evening = 17;
+    let currentHour = parseFloat(m.format("HH"));
+
+    if(currentHour >= split_afternoon && currentHour <= split_evening) {
+      greeting = "afternoon";
+    } else if(currentHour >= split_evening) {
+      greeting = "evening";
+    } else {
+      greeting = "morning";
+    }
+
+    return greeting;
   }
 
 }
