af319ebc748fd429f5a3721d1acd51f5593544f7
ali yeysides <ali.yeysides@gmail.com>
2017-05-19 08:56:18 -0400

    refactors greetingtime display logic

--- src/app/shared/primary-header/primary-header.component.ts
+++ src/app/shared/primary-header/primary-header.component.ts
@@ -19,25 +19,30 @@ export class PrimaryHeaderComponent implements OnInit {
 
   ngOnInit() {
     this.actionHeaderClass = {
-      'action-header__button--morning' : this.getGreetingTime() == 'morning',
-      'action-header__button--afternoon' : this.getGreetingTime() == 'afternoon',
-      'action-header__button--evening' : this.getGreetingTime() == 'evening'
+      'action-header__button--morning': this.getGreetingTime() == 'morning',
+      'action-header__button--afternoon': this.getGreetingTime() == 'afternoon',
+      'action-header__button--evening': this.getGreetingTime() == 'evening'
     }
   }
 
   public getGreetingTime() {
     let greeting = null,
-        m = moment();
+      m = moment();
 
-    if(!m || !m.isValid()) { return; }
+    if (!m || !m.isValid()) {
+      return;
+    }
 
-    let split_afternoon = 12;
-    let split_evening = 17;
-    let currentHour = parseFloat(m.format("HH"));
+    let split_afternoon = 12,
+        split_evening = 17,
+        currentHour = parseFloat(m.format("HH"));
 
-    if(currentHour >= split_afternoon && currentHour <= split_evening) {
+    console.log('currentHour', currentHour);
+    console.log('isEvening?', currentHour >= split_evening);
+
+    if (currentHour >= split_afternoon && currentHour < split_evening) {
       greeting = "afternoon";
-    } else if(currentHour >= split_evening) {
+    } else if (currentHour >= split_evening) {
       greeting = "evening";
     } else {
       greeting = "morning";
