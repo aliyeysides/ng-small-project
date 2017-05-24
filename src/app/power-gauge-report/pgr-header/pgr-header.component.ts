b360d6c10ef7aeeb75c739ac7ea987a42bc2ecf6
ali yeysides <ali.yeysides@gmail.com>
2017-05-24 15:35:22 -0400

    refactor

--- src/app/power-gauge-report/pgr-header/pgr-header.component.ts
+++ src/app/power-gauge-report/pgr-header/pgr-header.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {Location} from '@angular/common';
 
 import {SecondaryHeaderComponent} from "../../shared/secondary-header/secondary-header.component";
